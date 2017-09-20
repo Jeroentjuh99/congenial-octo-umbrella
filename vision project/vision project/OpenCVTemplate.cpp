909d5eb9046a8c42d59312228a1d72fed7c91719
Jeroentjuh99 <jeroenvanos2@gmail.com>
2017-09-20 08:51:22 +0200

    dit...

---
+++ vision project/vision project/OpenCVTemplate.cpp
@@ -0,0 +1,12 @@
+//Standard c++ includes
+#include "stdafx.h"
+#include <iostream>
+
+//OpenCV includes
+#include <opencv/cv.hpp>
+#include <opencv2/core.hpp>
+#include "blobdetectionavans.h"
+
+int main(int argc, char* argv[]) {
+	
+}
