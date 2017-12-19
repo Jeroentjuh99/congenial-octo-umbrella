82689d6d76adb8c513f87d6ab96eaa5a50bd47a4
Remco <remcosannen@gmail.com>
2017-12-19 16:10:01 +0100

    Updated Neural Network

--- Vision3/Vision3/VisionNN.cpp
+++ Vision3/Vision3/VisionNN.cpp
@@ -47,22 +47,40 @@ void VisionNN::capture_image()
 
 void VisionNN::train(double errorPercentage)
 {
+	cv::Mat_<int> train_labels(5,1);
+	train_labels(0) = 1;
+	train_labels(1) = 1;
+	train_labels(2) = 1;
+	train_labels(3) = 1;
+	train_labels(4) = 1;
+	cv::Mat confusion;
 	int categories = 5;
-	cv::Mat picture_data = cv::Mat::zeros(1, 4, CV_32F);
-	cv::ml::ANN_MLP *mlp = cv::ml::ANN_MLP::create();
-	cv::Mat_<int> layers(3, 1);
-	layers(0) = 8;
+	cv::Mat picture_data = cv::Mat::zeros(1, 5, CV_32F);
+	cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::create();
+	cv::Mat_<int> layers(4, 1);
+	layers(0) = 2;
 	layers(1) = 5;
 	layers(2) = 5;
+	layers(3) = 5;
 	mlp->setLayerSizes(layers);
 	mlp->setTermCriteria(cvTermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 300, 0.0001));
 	mlp->setTrainMethod(cv::ml::ANN_MLP::BACKPROP, 0.0001);
 	cv::Mat train_classes = cv::Mat::zeros(picture_data.rows, categories, CV_32FC1);
 	for (int i = 0; i<train_classes.rows; i++)
 	{
-		train_classes.at<float>(i, picture_data.at<int>(i)) = 1.f;
+		train_classes.at<float>(i, train_labels.at<int>(i)) = 1.f;
 	}
+	std::cout << picture_data << std::endl;
+	std::cout << train_classes << std::endl;
 	mlp->train(picture_data, cv::ml::ROW_SAMPLE, train_classes);
+	for (int i = 0; i<picture_data.rows; i++) {
+		int pred = mlp->predict(picture_data.row(i), cv::noArray());
+		int truth = picture_data.at<int>(i);
+		confusion.at<int>(pred, truth)++;
+	}
+	cv::Mat correct = confusion.diag();
+	float accuracy = sum(correct)[0] / sum(confusion)[0];
+	std::cout << accuracy << std::endl;
 }
 
 void VisionNN::get_objects()
