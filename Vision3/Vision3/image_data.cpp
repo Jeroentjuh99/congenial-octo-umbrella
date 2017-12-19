ba8549b17ec1be637f24114d275951197bdd239c
Remco <remcosannen@gmail.com>
2017-12-19 09:20:40 +0100

    Send pics

--- Vision3/Vision3/image_data.cpp
+++ Vision3/Vision3/image_data.cpp
@@ -6,6 +6,21 @@ image_data::image_data()
 {
 }
 
+double image_data::getBendingEnergy()
+{
+	return 0.0;
+}
+
+double image_data::getObjectSize()
+{
+	return 0.0;
+}
+
+double image_data::getHue()
+{
+	return 0.0;
+}
+
 
 image_data::~image_data()
 {
